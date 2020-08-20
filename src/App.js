12a70f6d0d9f8ae3c8596dba6ac44704d16d15f4
Piotr Fijałkowski <seejay12@gmail.com>
2020-08-20 17:51:11 +0200

    #remove tests files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloads.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
