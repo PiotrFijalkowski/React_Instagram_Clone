caecadfc10bfbd253ae672f9e7f478578a0b9881
Piotr Fijałkowski <seejay12@gmail.com>
2020-08-20 23:16:12 +0200

    #showing posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import './post.css';
 import Post from './Post';
 
 function App() {
@@ -10,6 +9,10 @@ function App() {
        <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="logo" className="app_headerImage"/>
      </div>
 
+     <Post username="Netciax" caption="Awsome! This Work!" imageUrl="https://images.squarespace-cdn.com/content/v1/52a1e3e4e4b055489471b02a/1534009601147-APZ9579DJIP8UAYO4WTL/ke17ZwdGBToddI8pDm48kL0xsO_zo2P7L8S-aIeZ7eJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmAf_4ymdGyABG5MXImgsXTGG5ECB_TAGzE9h9lclAO8vDkZL-Fi4FosXiNt0jW4w3/50+Engaging+Instagram+Post+Ideas+for+Online+Businesses+%7C+Elle+%26+Company"/>
+     <Post />
+     <Post />
+     <Post />
      <Post />
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
   return (
     <div className="post">
-        <h3>UserName</h3>
+      <div className="post__header">
+      <Avatar
+      className="post__avatar"
+      alt={username}
+      src="static/images/avatar/1.jpg"
+      >
+
+      </Avatar>
+        <h3>{username}</h3>
+
+      </div>
 
         <img className="post__image" src="https://images.squarespace-cdn.com/content/v1/52a1e3e4e4b055489471b02a/1534009601147-APZ9579DJIP8UAYO4WTL/ke17ZwdGBToddI8pDm48kL0xsO_zo2P7L8S-aIeZ7eJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmAf_4ymdGyABG5MXImgsXTGG5ECB_TAGzE9h9lclAO8vDkZL-Fi4FosXiNt0jW4w3/50+Engaging+Instagram+Post+Ideas+for+Online+Businesses+%7C+Elle+%26+Company" alt=""/>
     
-        <h4>Username: caption</h4>
+        <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
     </div>
   )
 }
