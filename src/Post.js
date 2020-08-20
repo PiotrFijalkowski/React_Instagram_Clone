1f6aa5237541f995d84080ca32c4b5d11c305fb0
Piotr Fijałkowski <seejay12@gmail.com>
2020-08-20 19:44:28 +0200

    #static one post and header

---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div className="post">
+        <h3>UserName</h3>
+
+        <img className="post__image" src="https://images.squarespace-cdn.com/content/v1/52a1e3e4e4b055489471b02a/1534009601147-APZ9579DJIP8UAYO4WTL/ke17ZwdGBToddI8pDm48kL0xsO_zo2P7L8S-aIeZ7eJ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmAf_4ymdGyABG5MXImgsXTGG5ECB_TAGzE9h9lclAO8vDkZL-Fi4FosXiNt0jW4w3/50+Engaging+Instagram+Post+Ideas+for+Online+Businesses+%7C+Elle+%26+Company" alt=""/>
+    
+        <h4>Username: caption</h4>
+    </div>
+  )
+}
+
+export default Post
